671fe66bcc47e6c9b55c1fe1c34d7fbbab03d04a
jnutterdev <hello@jnutterdev.com>
2024-11-25 12:02:17 -0500

    Setting up new longlost project

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "longlostforgotten";
+export const SITE_NAME = "longlostforgotten.com";
+export const SITE_URL = "https://longlostforgotten.com";
+export const SITE_DESCRIPTION = "Welcome to my website!";
